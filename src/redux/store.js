d3cb36e78dd8b6487b472a337cee8f43782135bd
Estani02 <estaniolmedo@gmail.com>
2023-05-26 23:50:37 -0300

    added slice to reducer in store

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import employeeSlice from './employeeSlice'
+import departmentsSlice from "./departmentsSlice";
+import positionSlice from "./positionSlice";
+import trainingSlice from "./trainingSlice";
+
+export const store = configureStore({
+    reducer: {
+        employee: employeeSlice,
+        departments: departmentsSlice,
+        position: positionSlice,
+        trainings: trainingSlice
+    }
+})
